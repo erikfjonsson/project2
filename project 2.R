17a841d7272944a79e6e260705f762e70328768a
erikfjonsson <erik.jonsson@live.com>
2018-03-01 08:03:14 +0100

    Update project 2.R

--- project 2.R
+++ project 2.R
@@ -3,24 +3,28 @@
 
 ## set up the files
 
-getwd() #get working directory
-setwd("C:/Users/erikj/Documents/764 local files/project 2") #set working directory
+#get working directory
+getwd()
 
-loansacc_original = read.csv("loans_accepted.csv", header=TRUE, sep=",", dec="." ) #import data with accepted loans
+#set working directory
+setwd("C:/Users/erikj/Documents/764 local files/project 2") 
 
+#get relevant libraries
+library(tree)
+library(randomForest)
 
-## create data frame
-
-loansacc = data.frame(loansacc_original, stringsAsFactors = TRUE)
+#import data with accepted loans
+loansacc = read.csv("loans_accepted.csv", header=TRUE, sep=",", dec="." )
 
 
 ## clean the data
-
-# change issue_d column to display only year instead of year and month, should we do this?
-# loansacc$issue_d = substring(loansacc$issue_d, 5, 9)
 
 # remove observations with unknown year
 loansacc = loansacc[!(loansacc$issue_d == ""),]
 
 # remove observations from 2007 due to changed credit model
 loansacc = loansacc[!(substring(loansacc$issue_d, 5, 9) == "2007"),]
+
+# change issue_d column to display only year instead of year and month and rename it, should we do this?
+# loansacc$issue_d = substring(loansacc$issue_d, 5, 9)
+# colnames(loansacc)[16] = "year"
